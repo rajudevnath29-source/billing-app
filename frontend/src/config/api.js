a623d74d370363eb2063fae459a62aec65a1b6ae
raju-devnath <raju.devnath@motion.ac.in>
2026-06-05 15:47:45 +0530

    resolved env issue

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,7 +1,4 @@
-export const API_BASE_URL = (
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5000"
-).replace(/\/$/, "");
-
+export const API_BASE_URL = 'https://billing-app-lwkd.onrender.com/' //'http://localhost:5000';
 export const API_URL = `${API_BASE_URL}/api`;
 
 export const getUploadUrl = (filename) => {
