9c2416fb238fef4723dfa60351112fc8e9a46f7d
Raju Home <raju.devnath29@gmail.com>
2026-06-05 22:34:55 +0530

    local api fix

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,11 +1,4 @@
-// export const API_BASE_URL = 'https://billing-app-lwkd.onrender.com' //'http://localhost:5000';
-const isLocalhost =
-  window.location.hostname === "localhost" ||
-  window.location.hostname === "127.0.0.1";
-
-export const API_BASE_URL = isLocalhost
-  ? "http://localhost:5000"
-  : "https://billing-app-lwkd.onrender.com";
+export const API_BASE_URL = 'https://billing-app-lwkd.onrender.com' //'http://localhost:5000';
 export const API_URL = `${API_BASE_URL}/api`;
 
 export const getUploadUrl = (filename) => {
